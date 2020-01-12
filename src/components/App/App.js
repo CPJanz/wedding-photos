dc6b09cc9154681bd01acc87d08e2d9fbdb626dc
Carl Janz <carl.janz@gmail.com>
2020-01-11 22:44:11 -0800

    Added routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Gallery from "../Gallery/Gallery";
+import Loading from "../Loading/Loading";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-      <Gallery />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Welcome</h1>
+        <React.Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path="/" component={Gallery} />
+            <Route render={() => <h1>404</h1>} />
+          </Switch>
+        </React.Suspense>
+      </div>
+    </Router>
   );
 }
 
