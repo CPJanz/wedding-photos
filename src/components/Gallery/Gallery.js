7d1883e59c95c6461249d0493290931fd884397d
Carl Janz <carl.janz@gmail.com>
2020-01-12 23:20:48 -0800

    Created add photo button

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,6 +2,9 @@ import React from "react";
 import Photo from "../Photo/Photo";
 import Loading from "../Loading/Loading";
 import ExpandedPhoto from "../ExpandedPhoto/ExpandedPhoto";
+import Scrim from "../Scrim/Scrim";
+import { FaPlusSquare, FaRegTimesCircle } from "react-icons/fa";
+import "./Gallery.css";
 
 const SAMPLE_NOTES = [
   ["Lorem ipsum"],
@@ -14,7 +17,7 @@ function generateSampleImages() {
   const SAMPLE_IMAGES = [];
   for (let i = 0; i < 10; i++) {
     SAMPLE_IMAGES.push({
-      url: `https://picsum.photos/200?random=${i}`,
+      url: `https://picsum.photos/200`,
       rotation: Math.floor(Math.random() * 16 - 8),
       note: SAMPLE_NOTES[Math.floor(Math.random() * SAMPLE_NOTES.length)]
     });
@@ -25,7 +28,9 @@ function generateSampleImages() {
 export default class Gallery extends React.Component {
   state = {
     images: null,
-    expandedPhoto: null
+    expandedPhoto: null,
+    addHover: "",
+    hoverClicked: false
   };
 
   componentDidMount() {
@@ -40,28 +45,51 @@ export default class Gallery extends React.Component {
     this.setState({ expandedPhoto: null });
   }
 
+  addClicked() {
+    this.setState({ hoverClicked: true });
+  }
+
+  addClosed() {
+    this.setState({ hoverClicked: false });
+  }
+
   render() {
-    const { images, expandedPhoto } = this.state;
-    if (images === null) {
-      return <Loading text="Fetching Photos" />;
-    } else {
-      return (
-        <div className="grid">
-          {images.map((image, key) => (
-            <Photo
-              key={`photo-${key}`}
-              {...image}
-              onClick={() => this.photoExpanded(image)}
-            />
-          ))}
-          {expandedPhoto && (
-            <ExpandedPhoto
-              {...expandedPhoto}
-              onClick={() => this.expandedClosed()}
-            />
-          )}
-        </div>
-      );
-    }
+    const { images, expandedPhoto, addHover, hoverClicked } = this.state;
+    return (
+      <React.Fragment>
+        {images === null ? (
+          <Loading text="Fetching Photos" />
+        ) : (
+          <div className="grid">
+            <div className="add-div">
+              <FaPlusSquare
+                className={`add-button ${addHover}`}
+                onMouseOver={() => this.setState({ addHover: "hovering" })}
+                onMouseOut={() => this.setState({ addHover: "" })}
+                onClick={() => this.addClicked()}
+              />
+            </div>
+            {images.map((image, key) => (
+              <Photo
+                key={`photo-${key}`}
+                {...image}
+                onClick={() => this.photoExpanded(image)}
+              />
+            ))}
+            {expandedPhoto && (
+              <ExpandedPhoto
+                {...expandedPhoto}
+                onClick={() => this.expandedClosed()}
+              />
+            )}
+            {hoverClicked && (
+              <Scrim>
+                {<FaRegTimesCircle onClick={() => this.addClosed()} />}
+              </Scrim>
+            )}
+          </div>
+        )}
+      </React.Fragment>
+    );
   }
 }
