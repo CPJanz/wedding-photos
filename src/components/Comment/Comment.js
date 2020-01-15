5709ec359c47cb69f7f38b66c8f00fb2b40850a3
Carl Janz <carl.janz@gmail.com>
2020-01-14 23:34:49 -0800

    Added ability to create new photo comments

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Comment.css";
 
-export default function Comment(comment) {
+export default function Comment(comment, key) {
   const { text, author } = comment;
   return (
-    <div className="comment-container">
+    <div className="comment-container" key={key}>
       <p className="comment">{text}</p>
       <p className="commentor">{`-${author}`}</p>
     </div>
--- src/components/ExpandedPhoto/ExpandedPhoto.js
+++ src/components/ExpandedPhoto/ExpandedPhoto.js
@@ -3,19 +3,39 @@ import "./ExpandedPhoto.css";
 import { FaRegTimesCircle } from "react-icons/fa";
 import Scrim from "../Scrim/Scrim";
 import Comment from "../Comment/Comment";
+import CommentInput from "../CommentInput/CommentInput";
 
-export default function ExpandedPhoto(photoInfo) {
-  const { url, note, onClick, comments } = photoInfo;
-  return (
-    <Scrim>
-      <div className="expanded-photo-container">
-        <img src={url} alt={"SOMETHING NEEDS TO BE HERE"} />
-        <p>{note.join(" ")}</p>
-        {comments.map(comment => Comment(comment))}
-        <span onClick={onClick}>
-          <FaRegTimesCircle />
-        </span>
-      </div>
-    </Scrim>
-  );
+export default class ExpandedPhoto extends React.Component {
+  state = {
+    comments: []
+  };
+
+  componentDidMount() {
+    this.setState({ comments: this.props.comments });
+  }
+
+  addComment = newComment => {
+    this.setState(state => {
+      return { comments: state.comments.concat(newComment) };
+    });
+    console.log("TODO: push new comment to the server.");
+  };
+
+  render() {
+    const { url, note, onClick } = this.props;
+    const { comments } = this.state;
+    return (
+      <Scrim>
+        <div className="expanded-photo-container">
+          <img src={url} alt={"SOMETHING NEEDS TO BE HERE"} />
+          <p>{note.join(" ")}</p>
+          {comments.map((comment, key) => Comment(comment, key))}
+          <CommentInput handleSubmit={result => this.addComment(result)} />
+          <span onClick={onClick}>
+            <FaRegTimesCircle />
+          </span>
+        </div>
+      </Scrim>
+    );
+  }
 }
