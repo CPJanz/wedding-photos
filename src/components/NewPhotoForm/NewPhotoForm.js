102743930a85ac88fa34690e4b2cc7b17bac46b1
Carl Janz <carl.janz@gmail.com>
2020-01-18 02:28:13 -0800

    Some layout work on NewPhotoForm

--- src/components/NewPhotoForm/NewPhotoForm.js
+++ src/components/NewPhotoForm/NewPhotoForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./NewPhotoForm.css";
-import { FaRegTimesCircle } from "react-icons/fa";
+import { FaRegTimesCircle, FaFileUpload } from "react-icons/fa";
 
 const SAMPLE_IMAGE = `https://picsum.photos/300/200`;
 
@@ -13,35 +13,43 @@ export default class NewPhotoForm extends React.Component {
     const { newPhoto } = this.state;
     return (
       <div className="new-photo-container">
-        <FaRegTimesCircle onClick={() => close()} />
-        <form
-          className=""
-          onSubmit={event => {
-            event.preventDefault();
-            console.log("submitting");
-            console.log(newPhoto);
-            submit(newPhoto);
-          }}
-        >
-          <input
-            type="text"
-            id="comment"
-            className="input note-input"
-            auto="off"
-            value={newPhoto.note}
-            onChange={event =>
-              this.setState({
-                newPhoto: {
-                  note: event.target.value,
-                  url: this.state.newPhoto.url
-                }
-              })
-            }
-          />
-          <button className="btn" type="submit" disabled={newPhoto.url === ""}>
-            Submit
-          </button>
-        </form>
+        <FaRegTimesCircle className="close-button" onClick={() => close()} />
+        <div className="new-photo-upload-node">
+          <FaFileUpload />
+          <form
+            className=""
+            onSubmit={event => {
+              event.preventDefault();
+              console.log("submitting");
+              console.log(newPhoto);
+              submit(newPhoto);
+              close();
+            }}
+          >
+            <input
+              type="text"
+              id="comment"
+              className="input note-input"
+              auto="off"
+              value={newPhoto.note}
+              onChange={event =>
+                this.setState({
+                  newPhoto: {
+                    note: event.target.value,
+                    url: this.state.newPhoto.url
+                  }
+                })
+              }
+            />
+            <button
+              className="btn"
+              type="submit"
+              disabled={newPhoto.url === ""}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
